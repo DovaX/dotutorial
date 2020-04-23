300fcaf566f5f0c64ca91db26884d57dee8c32e1
Dominik Vach <vach.dominik@gmail.com>
2020-04-23 21:01:31 +0200

    First program

---
+++ part1-print_input.py
@@ -0,0 +1,63 @@
+# print("Hello world!")
+
+# print("Anything")
+
+
+
+# print(1+1)
+
+# print(3-2)
+
+# print(4*8)
+
+
+# print(7/2)
+
+# print(3.14)
+
+
+# number1 = 7
+
+# print(number1)
+
+
+# number1 = 9
+
+# print(number1)
+
+
+# number2 = "2"
+
+
+# print(number2)
+
+
+# number2 = int(number2)
+
+# print(number1+number2)
+
+
+# number3 = "3"
+
+# number4 = "4"
+
+
+# print(number3 + number4)
+
+
+# number3 = int(number3)
+
+# number4 = int(number4)
+
+# print(number3 + number4)
+
+# number5 = str(number3 + number4)
+
+# number6 = input("Write a number \n")
+
+# print("Your number is:",number6)
+
+
+choice = input("What is your favourite colour? 1)Red 2)Yellow 3)Green 4)Blue 5)White 6)Black \n")
+
+print("Your favourite colour is:",choice)
