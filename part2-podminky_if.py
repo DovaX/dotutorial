9625de45401cbe62b1a0b060d6484d7e8cf0a69c
Dominik Vach <vach.dominik@gmail.com>
2020-05-02 23:03:21 +0200

    part2

---
+++ part2-podminky_if.py
@@ -0,0 +1,35 @@
+
+choice = input("What is your favourite colour? 1)Red 2)Yellow 3)Green 4)Blue 5)White 6)Black \n")
+
+
+if (choice == "1"):
+    print("Your favourite colour is: Red")
+    
+    
+if (choice == "2"):
+    print("Your favourite colour is: Yellow")
+    
+if (choice == "3"):
+    print("Your favourite colour is: Green")
+    
+if (choice == "4"):
+    print("Your favourite colour is: Blue")
+    
+if (choice == "5"):
+    print("Your favourite colour is: White")
+    
+if (choice == "6"):
+    print("Your favourite colour is: Black")
+    
+
+
+    
+
+    
+
+    
+
+    
+
+
+    
